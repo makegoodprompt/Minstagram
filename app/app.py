d6515abde993739c1ceaa2652562596739acbac0
makegoodprompt <contact@makegoodprompt.com>
2026-02-23 23:47:39 +0100

    [mgp] method to upload files

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.schemas import CreatePost, PostResponse
 from app.db import Post, create_database, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -13,7 +14,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/uplaod")
+@app.post("/upload")
 async def upload(
         file: UploadFile = File(...),
         caption: str = Form(""),
@@ -29,3 +30,21 @@ async def upload(
     await session.commit()
     await session.refresh(post)
     return post
+
+@app.get("/feed")
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session),
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+    posts_data = []
+    for post in posts:
+        posts_data.append({
+            "id": str(post.id),
+            "caption": post.caption,
+            "url": post.url,
+            "file_type": post.file_type,
+            "file_name": post.file_name,
+            "created_at": post.created_at.isoformat(),
+        })
+    return {"posts": posts_data}
