9820fe6dffe0e33797bb7b906d5dcdec853a17af
makegoodprompt <contact@makegoodprompt.com>
2026-02-23 23:28:47 +0100

    [mgp] Added db code

--- app/app.py
+++ app/app.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI, HTTPException
 from app.schemas import CreatePost, PostResponse
 from app.db import Post, create_database, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_database()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1:{
