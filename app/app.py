554b24eafb9be106502b43a8e57fc3b5672fab34
makegoodprompt <contact@makegoodprompt.com>
2026-02-23 23:36:30 +0100

    [mgp] Def for uploading dummy post

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import CreatePost, PostResponse
 from app.db import Post, create_database, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,3 +11,21 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/uplaod")
+async def upload(
+        file: UploadFile = File(...),
+        caption: str = Form(""),
+        session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url="some url",
+        file_type="Photo",
+        file_name="some file name",
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
